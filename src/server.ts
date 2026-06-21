bebbb935dc13953e9dcedf16215ae5c1fbb9c77c
Leonardo <leonardonabarro2003@gmail.com>
2026-06-21 18:53:02 -0300

    fix: add ignore to eslint

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app"
+import { env } from "./env"
 
 app.listen({
     host: "0.0.0.0",
-    port: process.env.PORT,
+    port: env.PORT,
 }).then(() => {
     console.log("HTTP server running on http://localhost:3333")
 }).catch(() => {
