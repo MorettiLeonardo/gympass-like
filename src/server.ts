3f64d68b0d8db5842487cd2acc6e1c6f802dbdcf
Leonardo <leonardonabarro2003@gmail.com>
2026-06-21 18:44:48 -0300

    config eslint, prettier and editor config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import { app } from './app'
+import { app } from "./app"
 
 app.listen({
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: process.env.PORT,
 }).then(() => {
-    console.log('HTTP server running on http://localhost:3333')
-}).catch(() => {    
-    console.error('Error starting the server:')
+    console.log("HTTP server running on http://localhost:3333")
+}).catch(() => {
+    console.error("Error starting the server:")
 })
