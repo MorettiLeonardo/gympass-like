5a2066ab9b965a970d63c9058f5766120df61fb3
Leonardo <leonardonabarro2003@gmail.com>
2026-06-21 18:33:09 -0300

    config env files

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,9 @@ import { app } from './app'
 
 app.listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: process.env.PORT,
 }).then(() => {
     console.log('HTTP server running on http://localhost:3333')
-}).catch((error) => {    
-    console.error('Error starting the server:', error)
+}).catch(() => {    
+    console.error('Error starting the server:')
 })
